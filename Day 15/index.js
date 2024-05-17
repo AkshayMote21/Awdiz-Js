22b8cc3d4bedfc5f497b9c6e3857d698d7150025
Akshayy <mote.akshay.18et1011@gmail.com>
2024-05-17 17:34:50 +0530

    updated..

--- Day 15/index.js
+++ Day 15/index.js
@@ -1,5 +1,5 @@
 // Binary Search
-
+            //  0   1  2  3  4  5  6 7   8  9
 var array = [11,12,13,14,15,16,17,18,19,20];
 var target = 19;
 function BinarySearch(array,target){
@@ -8,7 +8,7 @@ function BinarySearch(array,target){
     while(left <= right){                           // 0<=9 - true;     //5<=9 - true;     //8<=9-true;
         var midPoint = Math.floor((left+right)/2);  // (0+9)/2=4;       //(5+9)/2=7;       //(8+9)/2=8;
         if(array[midPoint] == target){              // 15==19-flase;    //18==19-false;    //19==19-true;
-            return midPoint;                                                               // return 8;
+            return midPoint;                         // return 8;
         }
         else if(array[midPoint] < target){          // 15<19-true;      //18<19-true;
             left = midPoint + 1;                    // left=4+1=5;      //left=7+1=8;
@@ -17,5 +17,9 @@ function BinarySearch(array,target){
             right = midPoint - 1;
         }
     }
+    // console.log(midPoint);
 }
 console.log(BinarySearch(array,target));
+
+
+
---
+++ Day 17/index.js
@@ -0,0 +1,21 @@
+// Promise
+function getData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const result = true;
+        if (result) {
+          resolve("Succeed!!!! Data recevied.");
+        } else {
+          reject("Failed!!!! Data not recevied .");
+        }
+      },3000 );
+    });
+  }
+  
+  getData()
+    .then((response) => {
+      console.log(response, "Response");
+    })
+    .catch((error) => {
+      console.error(error, "Error");
+    });
