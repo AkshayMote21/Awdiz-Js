0629066c4368d63d49e2c415d4e285be1321f2a6
Akshayy <mote.akshay.18et1011@gmail.com>
2024-05-18 10:05:31 +0530

    updated..

--- Day 17/index.js
+++ Day 17/index.js
@@ -2,7 +2,7 @@
 function getData() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        const result = true;
+        const result = false;
         if (result) {
           resolve("Succeed!!!! Data recevied.");
         } else {
@@ -19,3 +19,49 @@ function getData() {
     .catch((error) => {
       console.error(error, "Error");
     });
+
+
+// // Promise
+// function getData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const result = true;
+//       if (result) {
+//         resolve("Succeed!!!! Data recevied.");
+//       } else {
+//         reject("Failed!!!! Data not recevied .");
+//       }
+//     },3000 );
+//   });
+// }
+
+// getData()
+//   .then((response) => {
+//     console.log(response, "Response");
+//   })
+//   .catch((error) => {
+//     console.error(error, "Error");
+//   });
+
+
+// Promise
+// function getData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const result = true;
+//       if (result) {
+//         resolve("Succeed!!!! Data recevied.");
+//       } else {
+//         reject("Failed!!!! Data not recevied .");
+//       }
+//     },3000 );
+//   });
+// }
+
+// getData()
+//   .then((response) => {
+//     console.log(response, "Response");
+//   })
+//   .catch((error) => {
+//     console.error(error, "Error");
+//   });
